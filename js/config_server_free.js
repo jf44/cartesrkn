79c295f97444806444a9349afd47ef9a43c5868a
jfruitet <jean.fruitet@free.fr>
2020-12-25 23:04:17 +0100

    Signed-off-by: jfruitet <jean.fruitet@free.fr>

--- js/config_server_free.js
+++ js/config_server_free.js
@@ -1,7 +1,8 @@
 // JavaScript Document
-
+// le fichier utilisé doit être denommé config_server.js
 // Configuration for a local server se connectant au serveur de SailOnLine (SOL)  remote from the SOL server.
-
-// var rknserveururl ="http://localhost:8080/voilevirtuelle/vgv2020/";
+// Configuration pour un serveur distant sur Free.fr
+// Renommer ce fichier config_server.js pour la mise en exploitation
+// var rknserveururl ="http://localhost:8080/voilevirtuelle/vgv2020/carte_rkn/";
 var rknserveururl ="http://voilevirtuelle.free.fr/vgv2020/cartes_rkn/";
 
--- js/config_server_local.js
+++ js/config_server_local.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 // Configuration for a local server se connectant au serveur de SailOnLine (SOL)  remote from the SOL server.
-
-var rknserveururl ="http://localhost:8080/voilevirtuelle/vgv2020/ge/cartes_rkn/";
-// var rknserveururl ="http://voilevirtuelle.free.fr/vgv2020/";
+// Attention de renommer ce fichier config_server.js pour la mise en exploitation
+var rknserveururl ="http://localhost:8080/voilevirtuelle/vgv2020/cartes_rkn/";
+// var rknserveururl ="http://voilevirtuelle.free.fr/vgv2020/cartes_rekn/";
 
