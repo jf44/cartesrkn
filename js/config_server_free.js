c5a2cd4691fa5fd20b2226437b3592d78b55a648
jfruitet <jean.fruitet@free.fr>
2020-12-25 22:54:00 +0100

    Bugs corrections
    Signed-off-by: jfruitet <jean.fruitet@free.fr>

--- js/config_server_free.js
+++ js/config_server_free.js
@@ -3,5 +3,5 @@
 // Configuration for a local server se connectant au serveur de SailOnLine (SOL)  remote from the SOL server.
 
 // var rknserveururl ="http://localhost:8080/voilevirtuelle/vgv2020/";
-var rknserveururl ="http://voilevirtuelle.free.fr/vgv2020/ge/cartes_rkn/";
+var rknserveururl ="http://voilevirtuelle.free.fr/vgv2020/cartes_rkn/";
 
